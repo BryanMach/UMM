b05c9b62c8a45d40ba82689af4aa82f2451dfe3a
fabricio <fpanamamendez@gmail.com>
2024-05-15 16:32:29 -0400

    version 3.1 - Añadiendo certificado de registro

--- resources/views/admin/certificados/certificarregistro.blade.php
+++ resources/views/admin/certificados/certificarregistro.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,191 +12,228 @@
             padding: 20px;
             margin-top: 20px;
         }
-        .header, .footer {
+
+        .header,
+        .footer {
             text-align: center;
         }
+
         .section-title {
             background-color: rgb(209, 246, 252);
             text-align: center;
             padding: 5px;
         }
+
         .table {
-          border: 2px solid black;
+            border: 2px solid black;
             margin-bottom: 2px;
             text-align: center;
         }
-        .table td, .table th {
+
+        .table td,
+        .table th {
             padding: 0.5rem;
+            border: 1px solid black;
+            padding: 0;
+            margin: 0;
         }
+
         th {
-            background-color: rgb(209, 246, 252); /* Fondo celeste para la cabecera */
+            background-color: rgb(209, 246, 252);
+            /* Fondo celeste para la cabecera */
+        }
+
+        .texto {
+            line-height: 1.2;
+        }
+
+        .APropietario td {
+            white-space: nowrap;
+        }
+
+        .fuente {
+            font-family: 'Bookman Old Style';
+            font-size: 24px;
+        }
+
+        .fuente1 {
+            font-family: 'Bookman Old Style';
+            font-size: 20px;
         }
     </style>
 </head>
+
 <body>
 
-<div>
-    <div class="header">
-        <h5><strong>ESTADO PLURINACIONAL DE BOLIVIA</strong></h5>
-        <h6><strong>MINISTERIO DE DEFENSA</strong></h6>
-        <h6><strong>DIRECCIÓN GENERAL DE INTERESES MARÍTIMOS, FLUVIALES, LACUSTRES Y DE MARINA MERCANTE</strong></h6>
-        <div>
-          <table align="right" border=3>
-            <thead>
-                <tr>
-                    <th>N° REGISTRO</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="text-center">P-03240271</td>
-                </tr>
-            </tbody>
-        </table>
-      </div>
-        <h5><strong>CERTIFICADO DE REGISTRO</strong></h5>
-        <h6><strong>EXPEDIDO DE CONFORMIDAD AL RECONOCIMIENTO</strong></h6>
-        <h6><strong>EFECTUADA POR LA UNIDAD DE MARINA MERCANTE NACIONAL</strong></h6>
-        
-    </div>
-
-    <div class="text-center">
-        <table class="text-center">
-            <tbody>
-                <tr>
-                    <td>PROPIETARIO (S):</td>
-                    <td colspan="3"> <strong>ADMINISTRACION DE SERVICIOS PORTUARIOS-BOLIVIA (ASPB)</strong> </td>
-                </tr>
-                <tr>
-                    <td>ANTERIOR PROPIETARIO:</td>
-                    <td colspan="3"></td>
-                </tr>
-                <tr>
-                    <td>FECHA DE INSPECCIÓN:</td>
-                    <td colspan="3"><strong>21 DE MARZO DEL 2024</strong></td>
-                </tr>
-                <tr>
-                    <td>LUGAR DE INSPECCIÓN:</td>
-                    <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
-                </tr>
-                <tr>
-                    <td>BASE DE OPERACIONES:</td>
-                    <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>NOMBRE DE LA EMBARCACIÓN</strong></th>
-                    <th><strong>MATRICULA</strong></th>
-                    <th><strong>INDICATIVO DE LLAMADA</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>SOBERANIA I</td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>SERVICIOS DE LA EMBARCACIÓN</strong></th>
-                    <th><strong>TIPO</strong></th>
-                    <th><strong>AÑO DE CONSTRUCCIÓN</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>PONTON FLOTANTE</td>
-                    <td>ARTEFACTO NAVAL</td>
-                    <td>2023</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>ESLORA</strong></th>
-                    <th><strong>MANGA</strong></th>
-                    <th><strong>PUNTAL</strong></th>
-                    <th><strong>ARQUEO BRUTO</strong></th>
-                    <th><strong>ARQUEO NETO</strong></th>
-                    <th><strong>FRANCOBORDO</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>25.00<br>MTS.</td>
-                    <td>10.00<br>MTS.</td>
-                    <td>2.00<br>MTS.</td>
-                    <td>98.26<br>UNIDAD DE REGISTRO</td>
-                    <td>81.98<br>UNIDAD DE REGISTRO</td>
-                    <td>2.75<br>mm</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>TIPO Y MARCA DE<br> MOTOR</strong></th>
-                    <th><strong>NÚMERO DE<br> MOTOR</strong></th>
-                    <th><strong>POTENCIA <br>PROPULSIVA TOTAL</strong></th>
-                    <th><strong>POTENCIA ELÉCTRICA <br> NOMINAL TOTAL</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td> .-. </td>
-                    <td></td>
-                    <td> .-. </td>
-                    <td> .-. </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="text-center">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>MATERIAL DEL<br> CASCO</strong></th>
-                    <th><strong>PESO Y ALTURA<br> DE CUBIERTA</strong></th>
-                    <th><strong>MERCANCÍAS<br> PELIGROSAS</strong></th>
-                    <th><strong>NÚMERO MÁXIMO<br> DE PASAJEROS</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>ACERO</td>
-                    <td></td>
-                    <td>SI</td>
-                    <td>6</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
     <div>
-        <p><strong>SE CERTIFICA:</strong><br>
-        QUE LA EMBARCACIÓN, A LA FECHA DE REGISTRO CUMPLE CON LAS EXIGENCIAS DEL D.S. 12884 "LEY DE NAVEGACIÓN MARÍTIMA, FLUVIAL Y LACUSTRE" CAPÍTULO II, REGISTRO Y MATRÍCULA DE EMBARCACIONES Y CON LAS PRESCRIPCIONES PERTINENTES DE LA R.M. 0736 QUE APRUEBA EL REGLAMENTO DE REGISTRO DE BUQUES, EMBARCACIONES Y ARTEFACTOS NAVALES.</p>
-        <p>EL PRESENTE CERTIFICADO ES VÁLIDO POR CINCO AÑOS, CONFORME AL DECRETO SUPREMO N° 3073, A PARTIR DEL: 21/03/2024</p>
-        <p><strong>LUGAR Y FECHA:</strong> LA PAZ, 21 DE MARZO DE 2024</p>
-    </div>
-</div>
+        <div class="header">
+            <h5 class="fuente"><strong>ESTADO PLURINACIONAL DE BOLIVIA</strong></h5>
+            <h6 class="fuente"><strong>MINISTERIO DE DEFENSA</strong></h6>
+            <h6 class="fuente"><strong>DIRECCIÓN GENERAL DE INTERESES MARÍTIMOS, FLUVIALES, LACUSTRES Y DE MARINA
+                    MERCANTE</strong>
+            </h6>
+            <div>
+                <table align="right" border=3>
+                    <thead>
+                        <tr>
+                            <th>N° REGISTRO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">P-03240271</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h5 class="fuente"><strong>CERTIFICADO DE REGISTRO</strong></h5>
+            <h6 class="fuente1"><strong>EXPEDIDO DE CONFORMIDAD AL RECONOCIMIENTO</strong></h6>
+            <h6 class="fuente1"><strong>EFECTUADA POR LA UNIDAD DE MARINA MERCANTE NACIONAL</strong></h6>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
+        </div>
+
+        <div class="text-center">
+            <table class="text-center">
+                <tbody>
+                    <tr class="APropietario">
+                        <td>PROPIETARIO (S):</td>
+                        <td colspan="3"> <strong>ADMINISTRACION DE SERVICIOS PORTUARIOS-BOLIVIA (ASPB)</strong> </td>
+                    </tr>
+                    <tr class="APropietario">
+                        <td>ANTERIOR PROPIETARIO:</td>
+                        <td colspan="3"></td>
+                    </tr>
+                    <tr>
+                        <td>FECHA DE INSPECCIÓN:</td>
+                        <td colspan="3"><strong>21 DE MARZO DEL 2024</strong></td>
+                    </tr>
+                    <tr>
+                        <td>LUGAR DE INSPECCIÓN:</td>
+                        <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
+                    </tr>
+                    <tr>
+                        <td>BASE DE OPERACIONES:</td>
+                        <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>NOMBRE DE LA EMBARCACIÓN</strong></th>
+                        <th><strong>MATRICULA</strong></th>
+                        <th><strong>INDICATIVO DE LLAMADA</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>SOBERANIA I</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>SERVICIOS DE LA EMBARCACIÓN</strong></th>
+                        <th><strong>TIPO</strong></th>
+                        <th><strong>AÑO DE CONSTRUCCIÓN</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>PONTON FLOTANTE</td>
+                        <td>ARTEFACTO NAVAL</td>
+                        <td>2023</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>ESLORA</strong></th>
+                        <th><strong>MANGA</strong></th>
+                        <th><strong>PUNTAL</strong></th>
+                        <th><strong>ARQUEO BRUTO</strong></th>
+                        <th><strong>ARQUEO NETO</strong></th>
+                        <th><strong>FRANCOBORDO</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>25.00<br>MTS.</td>
+                        <td>10.00<br>MTS.</td>
+                        <td>2.00<br>MTS.</td>
+                        <td>98.26<br>UNIDAD DE REGISTRO</td>
+                        <td>81.98<br>UNIDAD DE REGISTRO</td>
+                        <td>2.75<br>mm</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>TIPO Y MARCA DE<br> MOTOR</strong></th>
+                        <th><strong>NÚMERO DE<br> MOTOR</strong></th>
+                        <th><strong>POTENCIA <br>PROPULSIVA TOTAL</strong></th>
+                        <th><strong>POTENCIA ELÉCTRICA <br> NOMINAL TOTAL</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> .-. </td>
+                        <td></td>
+                        <td> .-. </td>
+                        <td> .-. </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>MATERIAL DEL<br> CASCO</strong></th>
+                        <th><strong>PESO Y ALTURA<br> DE CUBIERTA</strong></th>
+                        <th><strong>MERCANCÍAS<br> PELIGROSAS</strong></th>
+                        <th><strong>NÚMERO MÁXIMO<br> DE PASAJEROS</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ACERO</td>
+                        <td></td>
+                        <td>SI</td>
+                        <td>6</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div>
+            <strong>SE CERTIFICA:</strong>
+            <p class="texto">
+                QUE LA EMBARCACIÓN, A LA FECHA DE REGISTRO CUMPLE CON LAS EXIGENCIAS DEL D.S. 12884 "LEY DE NAVEGACIÓN
+                MARÍTIMA, FLUVIAL Y LACUSTRE" CAPÍTULO II, REGISTRO Y MATRÍCULA DE EMBARCACIONES Y CON LAS
+                PRESCRIPCIONES PERTINENTES DE LA R.M. 0736 QUE APRUEBA EL REGLAMENTO DE REGISTRO DE BUQUES,
+                EMBARCACIONES Y ARTEFACTOS NAVALES.
+                <br>EL PRESENTE CERTIFICADO ES VÁLIDO POR CINCO AÑOS, CONFORME AL DECRETO SUPREMO N° 3073, A
+                PARTIR DEL:
+                21/03/2024
+            </p>
+            <p class="text-right"><strong>LUGAR Y FECHA:</strong> LA PAZ, 21 DE MARZO DE 2024</p>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
