e3abf824d5df10c404e3a1c7f52738dcaa9529b3
fabricio <fpanamamendez@gmail.com>
2024-05-15 17:49:57 -0400

    Version 3.3 - Correccion del certificado de seguridad

--- resources/views/admin/certificados/certificarseguridad.blade.php
+++ resources/views/admin/certificados/certificarseguridad.blade.php
@@ -1,212 +1,253 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Certificado de Seguridad de la Navegación</title>
+    <title>Certificado de Seguridad</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body {
-            line-height: 1.2;
-        }
         .container {
             border: 1px solid black;
             padding: 20px;
             margin-top: 20px;
         }
-        .header, .footer {
+
+        .header,
+        .footer {
             text-align: center;
         }
-        .header h5, .header h6 {
-            margin: 0;
-            font-size: 1rem;
-        }
-        .footer p {
-            font-size: 0.85rem;
-            margin: 5px 0;
-        }
+
         .section-title {
-            background-color: lightgray;
+            background-color: rgb(209, 246, 252);
             text-align: center;
             padding: 5px;
-            font-size: 0.9rem;
-            margin-bottom: 10px;
         }
+
         .table {
-            margin-bottom: 20px;
             border: 2px solid black;
+            margin-bottom: 2px;
+            text-align: center;
         }
-        .table-bordered td, .table-bordered th {
-            border: 2px solid black;
+
+        .table td,
+        .table th {
             padding: 0.5rem;
+            border: 1px solid black;
+            padding: 0;
+            margin: 0;
         }
+
         th {
-            background-color: rgb(209, 246, 252); /* Fondo celeste para la cabecera */
+            background-color: rgb(209, 246, 252);
+            /* Fondo celeste para la cabecera */
+        }
+
+        .texto {
+            line-height: 1.2;
+        }
+
+        .APropietario td {
+            white-space: nowrap;
+        }
+
+        .fuente {
+            font-family: 'Bookman Old Style';
+            font-size: 20px;
+        }
+
+        .fuente1 {
+            font-family: 'Bookman Old Style';
+            font-size: 15px;
+        }
+
+        .fuente2 {
+            font-size: 15px;
+        }
+
+        .text-center {
+            font-size: 15px;
         }
     </style>
 </head>
+
 <body>
 
-<div>
-    <div class="header">
-        <h5>ESTADO PLURINACIONAL DE BOLIVIA</h5>
-        <h6>MINISTERIO DE DEFENSA</h6>
-        <h6>DIRECCIÓN GENERAL DE INTERESES MARÍTIMOS, FLUVIALES, LACUSTRES Y DE MARINA MERCANTE</h6>
-        <div>
-            <table align="right" border=3>
-              <thead>
-                  <tr>
-                      <th>N° REGISTRO</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td class="text-center">P-03240271</td>
-                  </tr>
-              </tbody>
-          </table>
+    <div>
+        <div class="header">
+            <h5 class="fuente"><strong>ESTADO PLURINACIONAL DE BOLIVIA</strong></h5>
+            <h6 class="fuente"><strong>MINISTERIO DE DEFENSA</strong></h6>
+            <h6 class="fuente"><strong>DIRECCIÓN GENERAL DE INTERESES MARÍTIMOS, FLUVIALES, LACUSTRES Y DE MARINA
+                    MERCANTE</strong>
+            </h6>
+            <div>
+                <table align="right" border=3>
+                    <thead>
+                        <tr>
+                            <th>N° REGISTRO</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-center">P-03240271</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <h5 class="fuente"><strong>CERTIFICADO DE SEGURIDAD DE LA NAVEGACION</strong></h5>
+            <h6 class="fuente1"><strong>EXPEDIDO EN CUMPLIMIENTO AL D.S. Nº 3073</strong></h6>
+            <h6 class="fuente1"><strong>REGLAMENTO TECNICO A LA LEY N° 165 GENERAL DE TRANSPORTE EN LA</strong></h6>
+            <h6 class="fuente1"><strong>MODALIDAD DE TRANSPORTE ACUATICO, TITULO TERCERO, CAPITULO SEGUNDO ART.
+                    21</strong></h6>
+
         </div>
-        <h5>CERTIFICADO DE SEGURIDAD DE LA NAVEGACIÓN</h5>
-        <h6>EXPEDIDO EN CUMPLIMIENTO AL D.S. Nº 3073</h6>
-        <h6>REGLAMENTO TÉCNICO A LA LEY Nº 165 GENERAL DE TRANSPORTE EN LA MODALIDAD DE TRANSPORTE ACUÁTICO, TÍTULO TERCERO, CAPÍTULO SEGUNDO ART. 21</h6>
+
+        <div class="text-center">
+            <table class="text-center">
+                <tbody class="APropietario">
+                    <tr>
+                        <td>PROPIETARIO (S):</td>
+                        <td colspan="3"> <strong>ADMINISTRACION DE SERVICIOS PORTUARIOS-BOLIVIA (ASPB)</strong> </td>
+                    </tr>
+                    <tr>
+                        <td>ANTERIOR PROPIETARIO:</td>
+                        <td colspan="3"></td>
+                    </tr>
+                    <tr>
+                        <td>FECHA DE INSPECCIÓN:</td>
+                        <td colspan="3"><strong>21 DE MARZO DEL 2024</strong></td>
+                    </tr>
+                    <tr>
+                        <td>LUGAR DE INSPECCIÓN:</td>
+                        <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
+                    </tr>
+                    <tr>
+                        <td>BASE DE OPERACIONES:</td>
+                        <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>NOMBRE DE LA EMBARCACIÓN</strong></th>
+                        <th><strong>MATRICULA</strong></th>
+                        <th><strong>INDICATIVO DE LLAMADA</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>SOBERANIA I</td>
+                        <td>.-.</td>
+                        <td>.-.</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>SERVICIOS DE LA EMBARCACIÓN</strong></th>
+                        <th><strong>TIPO</strong></th>
+                        <th><strong>AÑO DE CONSTRUCCIÓN</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>PONTON FLOTANTE</td>
+                        <td>ARTEFACTO NAVAL</td>
+                        <td>2023</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>ESLORA</strong></th>
+                        <th><strong>MANGA</strong></th>
+                        <th><strong>PUNTAL</strong></th>
+                        <th><strong>ARQUEO BRUTO</strong></th>
+                        <th><strong>ARQUEO NETO</strong></th>
+                        <th><strong>FRANCOBORDO</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>25.00<br>MTS.</td>
+                        <td>10.00<br>MTS.</td>
+                        <td>2.00<br>MTS.</td>
+                        <td>98.26<br>UNIDAD DE REGISTRO</td>
+                        <td>81.98<br>UNIDAD DE REGISTRO</td>
+                        <td>2.75<br>mm</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>TIPO Y MARCA DE<br> MOTOR</strong></th>
+                        <th><strong>NÚMERO DE<br> MOTOR</strong></th>
+                        <th><strong>POTENCIA <br>PROPULSIVA TOTAL</strong></th>
+                        <th><strong>POTENCIA ELÉCTRICA <br> NOMINAL TOTAL</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> .-. </td>
+                        <td>.-.</td>
+                        <td> .-. </td>
+                        <td> .-. </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="text-center">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th><strong>MATERIAL DEL<br> CASCO</strong></th>
+                        <th><strong>PESO Y ALTURA<br> DE CUBIERTA</strong></th>
+                        <th><strong>MERCANCÍAS<br> PELIGROSAS</strong></th>
+                        <th><strong>NÚMERO MÁXIMO<br> DE PASAJEROS</strong></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>ACERO</td>
+                        <td>.-.</td>
+                        <td>SI</td>
+                        <td>6</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <br>
+        <div class="fuente2">
+            <strong>SE CERTIFICA:</strong>
+            <p class="texto">
+                QUE LA EMBARCACIÓN, HA SIDO OBJETO DE INSPECCIONES DE CONFORMIDAD CON LO PRESCRITO EN EL REGLAMETO DE
+                INSPECCIONES PARA EMBARCACIONES MERCANTES Y QUE DICHA INSPECCION, HA PUESTO DE MANIFIESTO QUE EL ESTADO
+                DE CASCO, LA ESTRUCTURA, MAQUINAS Y EL EQUIPO ES SATISFACTORIO Y QUE LA EMBARCACION CUMPLE CON LAS
+                PRESCRIPCIONES PERTINENTES EN LAS REGLAMENTACIONES VIGENTES.
+                <br>EL PRESENTE CERTIFICADO ES VÁLIDO POR <strong>CINCO AÑOS</strong>, CONFORME AL DECRETO SUPREMO N°
+                3073, A
+                PARTIR DEL:
+                21/03/2024 <br>
+                DEBIENDO SOMETERSE A LAS INSPECCIONES ANUALES OBLIGATORIAS EN LAS FECHAS ESTABLECIDAS POR LA AUTORIDAD
+                COMPETENTE.
+            </p>
+            <p class="text-right"><strong>LUGAR Y FECHA:</strong> LA PAZ, 21 DE MARZO DE 2024</p>
+        </div>
     </div>
 
-    <div class="table-responsive">
-        <table>
-            <tbody>
-                <tr>
-                    <td>PROPIETARIO (S):</td>
-                    <td colspan="3"><strong>ADMINISTRACION DE SERVICIOS PORTUARIOS-BOLIVIA (ASPB)</strong></td>
-                </tr>
-                <tr>
-                    <td>ANTERIOR PROPIETARIO:</td>
-                    <td colspan="3"><strong>0</strong></td>
-                </tr>
-                <tr>
-                    <td>FECHA DE INSPECCIÓN:</td>
-                    <td colspan="3"><strong>21 DE MARZO DEL 2024</strong></td>
-                </tr>
-                <tr>
-                    <td>LUGAR DE INSPECCIÓN:</strong></td>
-                    <td><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
-                </tr>
-                <tr>
-                    <td>BASE DE OPERACIONES:</td>
-                    <td colspan="3"><strong>PUERTO BUSCH - SANTA CRUZ</strong></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>NOMBRE DE LA EMBARCACIÓN</strong></th>
-                    <th><strong>MATRICULA</strong></th>
-                    <th><strong>INDICATIVO DE LLAMADA</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>SOBERANIA I</td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>SERVICIOS DE LA EMBARCACIÓN</strong></th>
-                    <th><strong>TIPO</strong></th>
-                    <th><strong>AÑO DE CONSTRUCCIÓN</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>PONTON FLOTANTE</td>
-                    <td>ARTEFACTO NAVAL</td>
-                    <td>2023</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>ESLORA </strong></th>
-                    <th><strong>MANGA </strong></th>
-                    <th><strong>PUNTAL </strong></th>
-                    <th><strong>ARQUEO BRUTO</strong></th>
-                    <th><strong>ARQUEO NETO</strong></th>
-                    <th><strong>FRANCOBORDO </strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>25.00<br>MTS.</td>
-                    <td>10.00<br>MTS.</td>
-                    <td>2.00<br>MTS.</td>
-                    <td>98.26<br>UNIDAD DE REGISTRO</td>
-                    <td>81.98<br>UNIDAD DE REGISTRO</td>
-                    <td>2.75<br>mm</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>TIPO Y MARCA DE<br> MOTOR</strong></th>
-                    <th><strong>NÚMERO DE<br> MOTOR</strong></th>
-                    <th><strong>POTENCIA<br>PROPULSIVA TOTAL</strong></th>
-                    <th><strong>POTENCIA ELÉCTRICA<br>NOMINAL TOTAL</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>.-.</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th><strong>MATERIAL DEL CASCO</strong></th>
-                    <th><strong>PESO Y ALTURA DE CUBIERTA</strong></th>
-                    <th><strong>MERCANCÍAS PELIGROSAS</strong></th>
-                    <th><strong>NÚMERO MÁXIMO DE PASAJEROS</strong></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>ACERO</td>
-                    <td></td>
-                    <td>SI</td>
-                    <td>6</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-
-    <div class="footer">
-        <p><strong>SE CERTIFICA:</strong><br>
-        QUE LA EMBARCACIÓN, HA SIDO OBJETO DE INSPECCIONES DE CONFORMIDAD CON LO ESTABLECIDO EN EL REGLAMENTO TÉCNICO, Y QUE A LA FECHA DEL REGISTRO SE CONSTATA QUE DICHA INSPECCIÓN, HA SIDO DE MANIFIESTO QUE EL ESTADO DEL CASCO, LA ESTABILIDAD, PROPULSIÓN, MAQUINARIA, Y SUS ELEMENTOS SATISFACEN TODAS LAS NORMAS EXIGIDAS, DISPONIÉNDOSE LAS PRESCRIPCIONES PERTINENTES EN LA LEGISLACIÓN VIGENTE.</p>
-        <p>EL PRESENTE CERTIFICADO ES VÁLIDO POR CINCO AÑOS, A PARTIR DEL: 21/03/2024</p>
-        <p>DEBIENDO SOMETERSE A LAS INSPECCIONES ANUALES OBLIGATORIAS EN LAS FECHAS ESTABLECIDAS POR LA AUTORIDAD COMPETENTE.</p>
-        <p  class="text-right"><strong>LUGAR Y FECHA:</strong> LA PAZ, 21 DE MARZO DE 2024</p>
-    </div>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
